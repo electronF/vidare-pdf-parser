03fbdb22bf5f11265c04880da8f781dcf23207d8
electronF <bertrandovide@gmail.com>
2023-08-09 03:12:41 +0100

    Upldate
    + DragAnd Drop

--- frontend/static/js/modules/uploading_section.js
+++ frontend/static/js/modules/uploading_section.js
@@ -1,82 +1,98 @@
-import UploadingItem from "../components/uploading_item.js"
+import UploadingItem from "../components/uploading_item.js";
 import saveFile from "../hooks/sendfile.js";
 import { fileVerificator, fileSize, fileName } from "../utils/function.js";
-import {POST_DOCUMENT} from "../constants.js";
+import { POST_DOCUMENT } from "../constants.js";
 import UploadedSection from "./uploaded_section.js";
 
+class UploadingSection {
 
-class UploadingSection
-{
-    constructor()
-    {
-        this.uploadedSection = new UploadedSection()
+  allowedTypes = [
+            'application/pdf', 
+            'application/vnd.oasis.opendocument.text', 
+            'application/vnd.ms-powerpoint', 
+            'application/vnd.openxmlformats-officedocument.presentationml.presentation'
+  ]
+  constructor() {
+    this.uploadedSection = new UploadedSection();
+  }
+
+  async uploadFile(file, path, uploadingItem) {
+    var response = await saveFile(file, path, POST_DOCUMENT);
+    if (response["success"] === false) {
+      alert(response["message"]);
+    } else {
+      uploadingItem.remove();
+      this.uploadedSection.addItem(response);
     }
+  }
 
-    async uploadFile(file, path, uploadingItem)
-    {
-        var response = await saveFile(file, path, POST_DOCUMENT)
-        if (response['success'] === false)
-        {
-            alert(response['message'])
-        }
-        else
-        {
-            uploadingItem.remove()  
-            this.uploadedSection.addItem(response)    
-        }
+  fileSaver = (files, paths) => {
+    var uploadedItems = $("#uploading-items");
+    var count = 0;
+    for (var file of files) {
+      var message = fileVerificator(file);
+      if (message === null) {
+        count++;
+        var uploadingItem = $(
+          new UploadingItem(
+            count,
+            fileName(file.name),
+            `${fileSize(file.size)}`,
+            file.type.split("/")[1]
+          ).render()
+        );
+        uploadedItems.append(uploadingItem);
+        this.uploadFile(file, paths[count - 1] ?? file.name, uploadingItem);
+      } else {
+        alert(message);
+      }
     }
+  };
 
-    #evenListenner(){
-        $('div.upload-section .browse-button').on('click', ()=>{
-            var uploadedItems = $('#uploading-items');
-            var inputFiles = $(`<input 
+  #evenListenner() {
+    $("div.upload-section .browse-button").on("click", () => {
+      var inputFiles = $(`<input 
                 type='file' 
                 accept='
-                    application/pdf, 
-                    application/vnd.oasis.opendocument.text, 
-                    application/vnd.ms-powerpoint, 
-                    application/vnd.openxmlformats-officedocument.presentationml.presentation
+                    ${this.allowedTypes.join(',')}
                     ' 
                 multiple 
-            />`)
-            inputFiles.click()
-            
-            inputFiles[0].onchange = ()=> {
-                var count = 0
-                var files = {}
-                var paths = inputFiles[0].value.split(';')
-                for(var file of inputFiles[0].files)
-                {
-                    var message = fileVerificator(file)
-                    if(message===null)
-                    {
-                        count++; 
-                        var uploadingItem = $(
-                            (new UploadingItem(
-                                count, 
-                                fileName(file.name), 
-                                `${fileSize(file.size)}`, 
-                                file.type.split('/')[1])
-                            ).render()
-                        )
-                        uploadedItems.append(uploadingItem)
-                        files[`file-${count}`] = {'file':file, 'path': paths[count-1]}
-                        this.uploadFile(file, paths[count-1], uploadingItem)
-                    } 
-                    else
-                    {
-                        alert(message)
-                    }   
-                }
-            }
-        })
-    }
+            />`);
+      inputFiles.click();
+      inputFiles[0].onchange = ()=> this.fileSaver(
+        inputFiles[0].files,
+        inputFiles[0].value.split(";")
+      );
+    });
 
-    listen ()
-    {
-        this.#evenListenner()
-    }
+    $("#upload-section").on("drop", (event) => {
+      event.preventDefault();
+
+      var droppedFiles = event.originalEvent.dataTransfer.files;
+      var paths = [];
+      var allowedFiles = [];
+
+      for (var file of droppedFiles) {
+        if(this.allowedTypes.includes(file.type))
+        {
+            paths.push(file.name);
+            allowedFiles.push(file)
+        }
+      }
+      this.fileSaver(allowedFiles, paths);
+    });
+
+    $("#upload-section").on(
+      "dragover dragenter dragover dragleave",
+      function (event) {
+        event.preventDefault();
+      }
+    );
+  }
+
+  listen() {
+    this.#evenListenner();
+  }
 }
 
-
-export default UploadingSection
+export default UploadingSection;
